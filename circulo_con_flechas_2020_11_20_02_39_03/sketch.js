d3e05b7361cd580d43b9c616a90ce4bea31dbfb8
MariaJesus242000 <70353890+MariaJesus242000@users.noreply.github.com>
2020-11-19 23:40:10 -0300

    Add files via upload

---
+++ circulo_con_flechas_2020_11_20_02_39_03/sketch.js
@@ -0,0 +1,77 @@
+ function setup() {
+      createCanvas(600, 300);
+    }
+      function draw() {
+      
+      background(0);
+      translate (mouseX, mouseY)
+      rotate(frameCount/ 50)
+      scale(frameCount/ 300)
+  
+      //noStroke()
+      fill(255,0,255,20) //color rojo
+      rect(0,-5,200,10) //color verde
+      ellipse(0,0,20,20) // color azul
+      triangle(200,-10,200,10,230,0) //color rojo
+  
+      fill(0,100,255, 80) //color rojo
+      rotate(frameCount/40) // color verde
+      rect(0,0,800,10) // color rojo
+      fill(255,0,255,20) //color rojo
+      rect(0,-5,200,10) //color verde
+      ellipse(0,0,20,20) // color azul
+      triangle(200,-10,200,10,230,0) //color rojo
+  
+      fill(0,100,255, 80) //color rojo
+      rotate(frameCount/40) // color verde
+      rect(0,0,800,10) // color rojo
+      fill(255,0,255,20) //color rojo
+      rect(0,-5,200,10) //color verde
+      ellipse(0,0,20,20) // color azul
+      triangle(200,-10,200,10,230,0) //color rojo
+  
+      fill(0,100,255, 80) //color rojo
+      rotate(frameCount/40) // color verde
+      rect(0,0,800,10) // color rojo
+      fill(255,0,255,20) //color rojo
+      rect(0,-5,200,10) //color verde
+      ellipse(0,0,20,20) // color azul
+      triangle(200,-10,200,10,230,0) //color rojo
+  
+      fill(0,100,255, 80) //color rojo
+      rotate(frameCount/40) // color verde
+      rect(0,0,800,10) // color rojo
+      fill(255,0,255,20) //color rojo
+      rect(0,-5,200,10) //color verde
+      ellipse(0,0,20,20) // color azul
+      triangle(200,-10,200,10,230,0) //color rojo
+  
+      fill(0,100,255, 80) //color rojo
+      rotate(frameCount/40) // color verde
+      rect(0,0,800,10) // color rojo
+      fill(255,0,255,20) //color rojo
+      rect(0,-5,200,10) // color rojo
+      ellipse(0,0,20,20) // color azul
+      triangle(200,-10,200,10,230,0) //color rojo
+  
+      fill(0,100,255, 80) //color rojo
+      rotate(frameCount/40) // color verde
+      rect(0,0,800,10) // color rojo
+      fill(255,0,255,20) //color rojo
+      rect(0,-5,200,10) //color verde
+      ellipse(0,0,20,20) // color azul
+      triangle(200,-10,200,10,230,0) //color rojo
+  
+      fill(0,100,255, 80) //color rojo
+      rotate(frameCount/40) // color verde
+      rect(0,0,800,10) // color rojo
+      fill(255,0,255,20) //color rojo
+      rect(0,-5,200,10) //color verde
+      ellipse(0,0,20,20) // color azul
+      triangle(200,-10,200,10,230,0) //color rojo
+  
+      fill(0,100,255, 80) //color rojo
+      rotate(frameCount/40) // color verde
+      rect(0,0,800,10) // color rojo
+      
+    } 
